2319a65f44892fe1ffe74f641af9b84879bbed01
luke89joseph <luke89joseph@gmail.com>
2025-06-02 17:05:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+document.getElementById("logoMain").onclick = function () {
+  window.location.href = "index.html";
+};
+document.getElementById("linkTO-about").onclick = function () {
+  window.location.href = "about.html";
+};
+document.getElementById("linkTO-fundraisers").onclick = function () {
+  window.location.href = "fundraisers.html";
+};
+document.getElementById("linkTO-contact").onclick = function () {
+  window.location.href = "contact.html";
+};
+document.getElementById("linkTO-resources").onclick = function () {
+  window.location.href = "resources.html";
+};
+
+document.getElementById("linkTO-aboutMOB").onclick = function () {
+  window.location.href = "about.html";
+};
+document.getElementById("linkTO-fundraisersMOB").onclick = function () {
+  window.location.href = "fundraisers.html";
+};
+document.getElementById("linkTO-contactMOB").onclick = function () {
+  window.location.href = "contact.html";
+};
+document.getElementById("linkTO-resourcesMOB").onclick = function () {
+  window.location.href = "resources.html";
+};
+document.getElementById("linkTO-insta").onclick = function () {
+  window.location.href = "https://www.instagram.com/bhsbearbots/";
+};
+
+document.getElementById("sitemapLINK").onclick = function () {
+  window.location.href = "sitemap.html";
+};
+
+document.getElementById("menu").onclick = function () {
+  console.log("=-enabled-=");
+  const mobBAR = document.getElementById("mobileNAVBAR");
+  if (mobBAR.classList.contains("hidebar")) {
+    mobBAR.classList.remove("hidebar");
+    if (document.getElementById("announcement") != null) {
+      document.getElementById("announcement").classList.add("hide");
+    }
+  } else {
+    mobBAR.classList.add("hidebar");
+    if (document.getElementById("announcement") != null) {
+      document.getElementById("announcement").classList.remove("hide");
+    }
+  }
+};
